02faa7cc71e1c6c0027dfa816d9f5e024e3c8251
Andrew <wty.andrew@gmail.com>
2024-08-10 11:35:08 +0800

    :arrow_up: Update dependencies + biome setup

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 
-import App from './App'
+import App from './app'
 
 describe('App', () => {
   it('renders correctly', () => {
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './app.tsx'
 import './assets/styles/main.css'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
