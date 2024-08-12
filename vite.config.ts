3f7d417abb8c15967dbd539e6a825638775e7bba
Andrew <wty.andrew@gmail.com>
2024-08-12 22:14:52 +0800

    :sparkles: Setup shadcn-ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest" />
 
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
