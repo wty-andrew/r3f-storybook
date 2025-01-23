835d1ab36b142f59e4d9bc228097cd9c294cf037
Andrew <wty.andrew@gmail.com>
2025-01-23 11:43:17 +0800

    :arrow_up: Upgrade dependencies

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,11 @@
 import type { Preview } from '@storybook/react'
 
+import '@/assets/styles/main.css'
+import './global.css'
+
 const preview: Preview = {
   parameters: {
+    layout: 'fullscreen',
     controls: {
       matchers: {
         color: /(background|color)$/i,
